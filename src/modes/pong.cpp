ddecc557fbc7fdcbd392fbcd38367df60b3bcf31
ninezerozeronine <25123779+ninezerozeronine@users.noreply.github.com>
2018-10-20 18:04:08 -0700

    First pass of update and draw functions

--- src/modes/pong.cpp
+++ src/modes/pong.cpp
@@ -10,18 +10,18 @@ Pong::Pong(CRGB* leds_) : Mode(leds_) {
 
 void Pong::initialise() {
     last_update = millis();
-    game_mode = WAIT_TO_START;
+    switch_to_wait_to_start();
 }
 
 void Pong::initialise_pot_0(int value) {
-    zone_size = map_pot_0_value(value);
+    set_zone_size(map_pot_0_value(value));
 }
 
 void Pong::initialise_pot_1(int value) {
-    top_hue = map_pot_to_byte(pot_value);
+    top_hue = map_pot_to_byte(value);
 }
 void Pong::initialise_pot_2(int value) {
-    bottom_hue = map_pot_to_byte(pot_value);
+    bottom_hue = map_pot_to_byte(value);
 }
 
 void Pong::initialise_button_1(byte state) {
@@ -41,79 +41,255 @@ void Pong::initialise_button_2(byte state) {
 
 void Pong::process_new_pot_0_value(int value) {
     if (game_mode == WAIT_TO_START) {
-        zone_size = map_pot_0_value(value);
+        set_zone_size(map_pot_0_value(value));
     }
 }
 
 void Pong::process_new_pot_1_value(int value) {
     if (game_mode == WAIT_TO_START) {
-        top_hue = map_pot_to_byte(value);
+        set_top_hue(map_pot_to_byte(value));
     }
 }
 
 void Pong::process_new_pot_2_value(int value) {
     if (game_mode == WAIT_TO_START) {
-        bottom_hue = map_pot_to_byte(value);
+        set_bottom_hue(map_pot_to_byte(value));
     }
 }
 
 void Pong::button_1_pressed() {
-    
+    top_was_pressed = true;
+    top_is_pressed = true;
 }
 
 void Pong::button_2_pressed() {
-
+    bottom_was_pressed = true;
+    bottom_is_pressed = true;
 }
 
 void Pong::button_1_released() {
-
+    top_is_pressed = false;
 }
 
 void Pong::button_2_released() {
-
+    bottom_is_pressed = false;
 }
 
 void Pong::update() {
+    current_millis = millis();
+    elapsed_millis = current_millis - last_update;
 
+    switch(game_mode){
+        case WAIT_TO_START:
+            update_wait_to_start();
+            break;
+        case COUNTDOWN:
+            update_countdown();
+            break;
+        case PLAYING:
+            update_playing();
+            break;
+        case TOP_WON:
+            update_top_won();
+            break;
+        case BOTTOM_WON:
+            update_bottom_won();
+            break;
+    }
+
+    // Reset button press detection
+    top_was_pressed = false;
+    bottom_was_pressed = false;
+
+    last_update = current_millis;
 }
 
 void Pong::apply_to_leds() {
-
+    switch(game_mode){
+        case WAIT_TO_START:
+            draw_wait_to_start();
+            break;
+        case COUNTDOWN:
+            draw_countdown();
+            break;
+        case PLAYING:
+            draw_playing();
+            break;
+        case TOP_WON:
+            draw_top_won();
+            break;
+        case BOTTOM_WON:
+            draw_bottom_won();
+            break;
+    }
 }
 
 
 void Pong::constructor_defaults(){
     last_update = 0;
+    elapsed_millis = 0;
     ball_speed = 0;
     ball_position = float(constants::GRID_HEIGHT)/2.0;
-    zone_size = 0.25
-    countdown_time = 0;
-    win_wipe_progress = 0;
+    ball_blink_state = true;
+    top_zone_min = 0;
+    top_zone_max = 0;
+    bottom_zone_min = 0;
+    bottom_zone_max = 0;
+    countdown_begin = 0;
+    win_begin = 0;
     top_hue = 0;
     bottom_hue = 180;
     both_pressed_begin = 0;
 
-    game_mode WAIT_TO_START;
+    game_mode = WAIT_TO_START;
+    both_pressed = false;
     top_was_pressed = false;
     top_is_pressed = false;
     bottom_was_pressed = false;
     bottom_is_pressed = false;
 }
 
-
-
-
-
-
-void Pong::process_new_pot_0_value(int value) {
-    
-}
-
 int Pong::map_pot_0_value(int value) {
     float percentage = float(value)/1023.0;
-    return (ZONE_SIZE_MIN + (1.0 - percentage)) + (ZONE_SIZE_MAX * percentage);
+    return (ZONE_PERCENT_MIN + (1.0 - percentage)) + (ZONE_PERCENT_MAX * percentage);
 }
 
 byte Pong::map_pot_to_byte(int pot_value) {
     return map(pot_value, 0, 1023, 0, 255);
+}
+
+void Pong::set_zone_size(float percentage) {
+    top_zone_min = 0.0;
+    top_zone_max = (constants::GRID_HEIGHT * 0.5) * percentage;
+
+    bottom_zone_min = constants::GRID_HEIGHT - top_zone_max;
+    bottom_zone_max = constants::GRID_HEIGHT;
+}
+
+void Pong::set_top_hue(byte hue) {
+    top_hue = hue;
+}
+
+void Pong::set_bottom_hue(byte hue) {
+    bottom_hue = hue;
+}
+
+void Pong::update_wait_to_start() {
+    // Calculate blink
+    int blink_period_progress = current_millis % 1000;
+    if (blink_period_progress > 500) {
+        ball_blink_state = true;
+    } else {
+        ball_blink_state = false;
+    }
+
+    // Test if both buttons have been pressed for long enough
+    if (top_is_pressed and bottom_is_pressed) {
+        if (both_pressed) {
+            if (current_millis - both_pressed_begin > 1000) {
+                switch_to_countdown();
+            }
+        } else {
+            both_pressed = true;
+            both_pressed_begin = current_millis;
+        }
+    } else {
+        both_pressed = false;
+    }
+}
+
+void Pong::update_countdown() {
+    if ((current_millis - countdown_begin) > 2000) {
+        switch_to_playing();
+    }
+}
+
+void Pong::update_playing() {
+    if (ball_position < 0) {
+        switch_to_bottom_won();
+    }
+
+    if (ball_position > float(constants::GRID_HEIGHT)) {
+        switch_to_top_won();
+    }
+
+    // Check if top has returned
+    if (ball_speed < 0) {
+        if (ball_position < top_zone_max) {
+            if (top_was_pressed) {
+                return_ball();
+            }
+        }
+    }
+
+    // Check if bottom has returned
+    if (ball_speed > 0) {
+        if (ball_position > bottom_zone_min) {
+            if (bottom_was_pressed) {
+                return_ball();
+            }
+        }
+    }
+
+    // Update ball position
+    ball_position = ball_position + (ball_speed * elapsed_millis);
+}
+
+void Pong::update_top_won() {
+    if ((current_millis - win_begin) > 2000) {
+        switch_to_wait_to_start();
+    }
+}
+
+void Pong::update_bottom_won() {
+    if ((current_millis - win_begin) > 2000) {
+        switch_to_wait_to_start();
+    }
+}
+
+void Pong::draw_wait_to_start() {
+    FastLED.showColor(CRGB::Black);
+    draw_endzones();
+    if (ball_blink_state) {
+        draw_ball();
+    }
+
+}
+
+void Pong::draw_countdown() {
+    draw_endzones();
+    draw_ball();
+}
+
+void Pong::draw_playing() {
+    if (elapsed_millis > FADE_INTERVAL) {
+        for(int index = 0; index < constants::NUM_LEDS; index++) {
+            leds[index].fadeToBlackBy(FADE_AMOUNT);
+        }
+    draw_endzones();
+    draw_ball();
+}
+
+void Pong::draw_top_won() {
+
+}
+
+void Pong::set_all_black() {
+
+}
+
+void Pong::draw_bottom_won() {
+
+}
+
+void Pong::fade_all() {
+
+}
+
+void Pong::draw_endzones() {
+
+}
+
+void Pong::draw_ball() {
+
 }
--- src/modes/pong.h
+++ src/modes/pong.h
@@ -5,8 +5,10 @@
 #include "mode_base.h"
 #include "constants.h"
 
-#define ZONE_SIZE_MIN 0.1
-#define ZONE_SIZE_MAX 0.5
+#define ZONE_PERCENT_MIN 0.1
+#define ZONE_PERCENT_MAX 0.5
+#define FADE_INTERVAL 10
+#define FADE_AMOUNT 100
 
 class Pong: public Mode {
     public:
@@ -45,35 +47,50 @@ class Pong: public Mode {
 
         void set_zone_size(float percentage);
         void set_top_hue(byte hue);
-        void set_bottom_hue(bye_hue);
+        void set_bottom_hue(byte hue);
 
         void update_wait_to_start();
         void update_countdown();
         void update_playing();
         void update_top_won();
         void update_bottom_won();
+        void return_ball();
 
         void draw_wait_to_start();
         void draw_countdown();
         void draw_playing();
         void draw_top_won();
         void draw_bottom_won();
+        void set_all_black();
+        void fade_all();
+        void draw_endzones();
+        void draw_ball();
 
         void switch_to_wait_to_start();
         void switch_to_countdown();
         void switch_to_playing();
-        void switch_to_top_wins();
-        void switch_to_bottom_wins();
+        void switch_to_top_won();
+        void switch_to_bottom_won();
+
+
+
 
         unsigned long last_update;
+        unsigned long elapsed_millis;
+        unsigned long current_millis;
         float ball_speed;
         float ball_position;
-        float zone_size;
-        int countdown_time;
-        float win_wipe_progress;
+        bool ball_blink_state;
+        float top_zone_min;
+        float top_zone_max;
+        float bottom_zone_min;
+        float bottom_zone_max;
+        unsigned long countdown_begin;
+        unsigned long win_begin;
         int top_hue;
         int bottom_hue;
         unsigned long both_pressed_begin;
+        bool both_pressed;
 
         game_mode_t game_mode;
         bool top_was_pressed;
